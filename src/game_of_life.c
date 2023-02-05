b50a42e62948100f4b636c1c324c1ea1d622fed0
Wheat Welker <wheatwel@si-a1.kzn.21-school.ru>
2023-02-05 14:10:54 +0300

    for tests

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,64 +1,81 @@
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <ncurses.h>
 
 #define width 80
 #define height 25
+#define max_speed 20
 
-void input(int** uni);
-int **next_generation(int **universe);
-int alive_neighbour(int **universe, int row, int column);
-void copy(int **old, int **new);
-void output(int **matrix);
+void input(int uni[height][width]);
+void next_generation(int universe[height][width], int update[height][width]);
+int alive_neighbour(int universe[height][width], int row, int column);
+void copy(int old[height][width], int new[height][width]);
+void output(int universe[height][width]);
 void key(int *speed);
+int stop(int old[height][width], int new[height][width]);
 
 int main() {
+    int delay = max_speed / 2;
+    int universe[height][width];
+    input(universe);
     initscr();
     noecho();
     nodelay(stdscr, true);
-    int delay = 1;
-    int **universe = calloc(height, sizeof(int *));
-    for (int i = 0; i < height; i++) {
-        universe[i] = calloc(width, sizeof(int));
-    }
-    input(universe);
-    freopen("/dev/tty","r", stdin);
+    freopen("/dev/tty", "r", stdin);
+    int nuniverse[height][width];
     while (1) {
         output(universe);
-        printw("%d", delay);
+
+        printw("current speed: %d", max_speed - delay);
         refresh();
         key(&delay);
-        usleep(delay * 100000); 
-        int **nuniverse = next_generation(universe);
+        usleep(delay * 50000);
+        next_generation(universe, nuniverse);
+        if (stop(universe, nuniverse)) {
+            break;
+        }
         copy(nuniverse, universe);
-        
     }
-    endwin();
+   // endwin();
     return 0;
 }
 
 void key(int *speed) {
     char c = getch();
-    if (c == '-' && (*speed) > 0) {
+    if (c == '+' && (*speed) > 1) {
         (*speed)--;
     }
-    if (c == '+') {
+    if (c == '-' && (*speed) < 20) {
         (*speed)++;
     }
 }
 
-void copy(int **old, int **new) {
+int stop(int old[height][width], int new[height][width]) {
+    int flag = 1;
+    int count = 0;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (new[i][j] == 1) {
+                count++;
+            }
+            if (new[i][j] != old[i][j]) {
+                flag = 0;
+            }
+        }
+    }
+    return flag || !count;
+}
+
+void copy(int old[height][width], int new[height][width]) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             new[i][j] = old[i][j];
         }
-        free(old[i]);
     }
-    free(old);
 }
 
-void input(int **uni) {
+void input(int uni[height][width]) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             scanf("%d", &uni[i][j]);
@@ -66,15 +83,17 @@ void input(int **uni) {
     }
 }
 
-int **next_generation(int **universe) {
-    int **update = calloc(height, sizeof(int *));
+void next_generation(int universe[height][width], int update[height][width]) {
     for (int i = 0; i < height; i++) {
-        update[i] = calloc(width, sizeof(int));
+        for (int j = 0; j < width; j++) {
+            update[i][j] = 0;
+        }
     }
+
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             int a = alive_neighbour(universe, i, j);
-
+            printw("%d", a);
             if (universe[i][j] == 1) {
                 if (a == 2 || a == 3) update[i][j] = 1;
 
@@ -85,16 +104,16 @@ int **next_generation(int **universe) {
                 update[i][j] = 0;
             }
         }
+        printw("\n");
     }
-    return update;
 }
 
-int alive_neighbour(int **universe, int row, int column) {
+int alive_neighbour(int universe[height][width], int row, int column) {
     int count = 0;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
             int indexI = (row + i + height) % height;
-            int indexJ = (column + j + width)  % width;
+            int indexJ = (column + j + width) % width;
             if (universe[indexI][indexJ] == 1) count += 1;
         }
     }
@@ -103,14 +122,13 @@ int alive_neighbour(int **universe, int row, int column) {
     return count;
 }
 
-void output(int **matrix) {
+void output(int matrix[height][width]) {
     clear();
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             printw("%c", matrix[i][j] == 0 ? ' ' : '#');
-
         }
 
-    printw("\n");
+        printw("\n");
     }
 }
